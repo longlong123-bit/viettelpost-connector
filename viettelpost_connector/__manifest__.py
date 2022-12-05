9362522f245c62a32f04158b72ceea1694c16af9
Long Duong Nhat <l99store.long@gmail.com>
2022-12-05 14:44:58 +0700

    Hot fix thumbnail and logo

--- viettelpost_connector/__manifest__.py
+++ viettelpost_connector/__manifest__.py
@@ -1,10 +1,10 @@
 {
-    'name': 'ViettelPost Connector',
+    'name': 'Odoo Viettel Post Connector',
     'version': '15.0.1.0',
-    'summary': 'Connect Odoo Application with ViettelPost',
+    'summary': 'Connect Odoo Application with Viettel Post',
     'description': """
-        The Odoo ViettelPost Connector module is an integrated product between the odoo application and the carrier ViettelPost. 
-        The application provides features, which through the api to manipulate directly into the dashboard of ViettelPost.
+        The Odoo Viettel Post Connector module is an integrated product between the odoo application and the carrier Viettel Post. 
+        The application provides features, which through the api to manipulate directly into the dashboard of Viettel Post.
     """,
     'category': 'Services/Connector',
     'support': 'odoo.tangerine@gmail.com',
